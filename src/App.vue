1ed30e12d915038e67913ed7d3af00b3f8e5af80
rry <r.yamakuchi@gmail.com>
2022-02-24 20:07:03 +0900

    feat: v-if で表示・非表示を切り替える

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,9 @@
     <template
       v-for="item in items"
       :key="item.id">
-      <div class="item">
+      <div
+        v-if="!item.soldOut"
+        class="item">
         <div class="thumbnail">
           <img
             :src="item.image"
@@ -37,7 +39,8 @@ export default {
           description:
             '刻んだ野菜をアボカドと混ぜてディップに。こんがり焼いたバゲットとお召し上がりください。',
           price: 480,
-          image: '/images/item1.jpg'
+          image: '/images/item1.jpg',
+          soldOut: false
         },
         {
           id: 2,
@@ -45,7 +48,8 @@ export default {
           description:
             '子供のころに食べたかった、あのホットケーキを再現しました。素朴でどこか懐かしい味をどうぞ。',
           price: 1180,
-          image: '/images/item2.jpg'
+          image: '/images/item2.jpg',
+          soldOut: false
         },
         {
           id: 3,
@@ -53,7 +57,8 @@ export default {
           description:
             'ロサンゼルス生まれのスパークリングウォーター。ノンカロリー、ノンアルコールの新感覚飲料です。',
           price: 320,
-          image: '/images/item3.jpg'
+          image: '/images/item3.jpg',
+          soldOut: true
         },
         {
           id: 4,
@@ -61,7 +66,8 @@ export default {
           description:
             'イタリア産チーズをたっぷりかけたアツアツのフレンチフライ。みんな大好きな一品です。',
           price: 670,
-          image: '/images/item4.jpg'
+          image: '/images/item4.jpg',
+          soldOut: false
         }
       ]
     }
